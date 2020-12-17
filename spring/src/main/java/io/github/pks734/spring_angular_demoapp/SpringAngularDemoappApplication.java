842fbb59164d8693d4f56acfb970a4c1bf00ef08
ksamodol <p7k3s4@gmail.com>
2020-12-17 22:18:54 +0100

    initial commit

---
+++ spring/src/main/java/io/github/pks734/spring_angular_demoapp/SpringAngularDemoappApplication.java
@@ -0,0 +1,13 @@
+package io.github.pks734.spring_angular_demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularDemoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularDemoappApplication.class, args);
+    }
+
+}
---
+++ spring/src/test/java/io/github/pks734/spring_angular_demoapp/SpringAngularDemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pks734.spring_angular_demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularDemoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
